db262aa658dc4196b59ca1ee8814d4b61bcdafd6
Jack Chakany <jack@chakany.systems>
2024-12-07 18:28:50 -0500

    add live-updating zap counter to events

--- src/components/Recipe/TotalZaps.svelte
+++ src/components/Recipe/TotalZaps.svelte
@@ -12,20 +12,21 @@
   let didSigs = new Map();
 
   async function fetch() {
-    const evs = await $ndk.fetchEvents({
+    const sub = await $ndk.subscribe({
       kinds: [9735],
       '#a': [`${event.kind}:${event.author.pubkey}:${event.tags.find((e) => e[0] == 'd')?.[1]}`]
-    });
-    evs.forEach((a) => {
-      let bolt11 = a.tags.find((e) => e[0] == 'bolt11')?.[1];
-      if (bolt11 && a.sig) {
-        if (!didSigs.has(a.sig)) {
+    }, { closeOnEose: false });
+    sub.on("event", (event: NDKEvent) => {
+      let bolt11 = event.tags.find((e) => e[0] == 'bolt11')?.[1];
+      if (bolt11 && event.sig) {
+        if (!didSigs.has(event.sig)) {
           let decoded = decode(bolt11);
           totalZapAmount =
             totalZapAmount + Number(decoded.sections.find((e) => e.name == 'amount').value);
-          didSigs.set(a.sig, a.sig);
+          didSigs.set(event.sig, event.sig);
         }
       }
+
     });
   }
 
