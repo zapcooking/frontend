9f0a1abfe98b731f47989f28506038faa3a03d58
The Daniel <dmnyc@users.noreply.github.com>
2026-01-26 20:53:40 -0500

    Add 3 more placeholder images for recipes and articles

--- src/lib/placeholderImages.ts
+++ src/lib/placeholderImages.ts
@@ -4,8 +4,10 @@
  */
 
 const PLACEHOLDER_IMAGES = [
-  'https://image.nostr.build/87c3e4e4c3e24c268106e7352eaa94538815fab6d4104903b9d82fbe4464e7d5.jpg'
-  // Add more placeholder images here as needed
+  'https://image.nostr.build/87c3e4e4c3e24c268106e7352eaa94538815fab6d4104903b9d82fbe4464e7d5.jpg',
+  'https://image.nostr.build/a84c0743f50c51b8956f28350da73ba06106529219d33aead1cbb8b96ac3554f.jpg',
+  'https://image.nostr.build/2e967f1520054c91b0320b73e665ad1b685dcc0626883fc0654889463d56e7ac.jpg',
+  'https://image.nostr.build/0dc0f817ea420b9fc7fc7a5ce83a01f3cf52ba8e8c26edc121a1d8d496189291.jpg'
 ];
 
 /**
